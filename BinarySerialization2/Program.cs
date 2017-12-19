97b93a60ccdb61e9c42f7c183d49cf9179db1065
kasper <kasper.graversen@formpipe.com>
2017-12-19 08:07:55 +0100

    Class v1

---
+++ BinarySerialization2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Data;
+
+namespace BinarySerialization2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\data";
+
+            if (Class1.Version == 1)
+            {
+                Console.WriteLine("Writing data");
+                File.WriteAllBytes(path, SerializeHelper<Class1>.Serialize(new Class1() { SomeInt = 42, SomeString = "HelloWorld", Next = new Class1() { SomeInt = 3, SomeString = "Boo" } }));
+            }
+
+            Console.WriteLine(SerializeHelper<Class1>.Deserialize(File.ReadAllBytes(path)).ToString());
+        }
+    }
+
+    public class SerializeHelper<T>
+    {
+        public static byte[] Serialize(T obj)
+        {
+            using (var stream = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(stream, obj);
+                return stream.ToArray();
+            }
+        }
+
+        public static T Deserialize(byte[] bytes)
+        {
+            using (var stream = new MemoryStream())
+            {
+                stream.Write(bytes, 0, bytes.Length);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)new BinaryFormatter().Deserialize(stream);
+            }
+        }
+    }
+}
---
+++ Data2/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Data
+{
+    [Serializable]
+    public class Class1
+    {
+        public const int Version = 1;
+
+        public string SomeString;
+        public int SomeInt;
+        public Class1 Next;
+        
+        public override string ToString()
+        {
+            return SomeString + " " + SomeInt + " | "+Next?.ToString();
+        }
+    }
+}
